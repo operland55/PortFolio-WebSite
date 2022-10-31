0b2f9acfba14d391e4dc1fc1787b3363afc531eb
jungpa <operland55@naver.com>
2022-10-31 22:30:35 +0900

    ✨ Header Component

---
+++ src/Components/Header.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { useRecoilState } from "recoil";
+import { Link } from "react-router-dom";
+import Logo from "../assets/logo.png";
+import { BsSearch } from "react-icons/bs";
+import { BiMenu } from "react-icons/bi";
+import Menu from "./Menu";
+import { MenuToggle } from "../atom";
+function Header() {
+	const [menuBtn, setMenuBtn] = useRecoilState(MenuToggle);
+
+	const onClick = () => {
+		setMenuBtn((toggle) => !toggle);
+	};
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-sm-4">
+					<div className="global-header">
+						<div className="global-header-container">
+							<div className="row">
+								<div className="col-sm-4">
+									<header className="gnb-header">
+										<div className="logo sm-only">
+											<Link to="/">
+												<img src={Logo} alt="logo" />
+											</Link>
+										</div>
+
+										<div className="header-icon sm-only">
+											<button className="header-search">
+												<Link to="/search">
+													<BsSearch />
+												</Link>
+											</button>
+											<button className="header-menu">
+												<BiMenu onClick={onClick} />
+											</button>
+										</div>
+
+										<div className="header-top md-only">
+											<p>Login</p>
+											<p>Service center</p>
+										</div>
+
+										<div className="header-bottom md-only">
+											<nav className="header-bottom-list">
+												<h1 className="visually-hidden">Menu</h1>
+
+												<div className="header-bottom-nav">
+													<div className="header-bottom-logo">
+														<Link to={"/"}>
+															<img src={Logo} alt="Logo" />
+														</Link>
+													</div>
+
+													<ul className="header-bottom-item">
+														<li className="header-bottom-items">
+															<Link to={"/"}>Home</Link>
+														</li>
+														<li>
+															<Link to={"/Viewall"}>View all</Link>
+														</li>
+														<li className="header-bottom-items">
+															<Link to={"/Favorites"}>Favorites</Link>
+														</li>
+														<li className="header-bottom-items">
+															<Link to={"/Advertisement"}>Advertisement</Link>
+														</li>
+													</ul>
+												</div>
+											</nav>
+
+											<div className="header-bottom-search">
+												<Link to="/search">
+													<BsSearch /> Search
+												</Link>
+											</div>
+										</div>
+									</header>
+								</div>
+							</div>
+						</div>
+
+						{menuBtn ? <Menu /> : ""}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
