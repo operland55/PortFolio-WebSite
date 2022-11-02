ee9f6f381e990f5fb9f6773d5f40e954465b0e63
jungpa <operland55@naver.com>
2022-11-02 20:21:52 +0900

    📝 viedo toggle Add

---
+++ src/atom.tsx
@@ -0,0 +1,10 @@
+import { atom } from "recoil";
+
+export const MenuToggle = atom({
+	key: "MenuBtn",
+	default: false,
+});
+export const VideoToggle = atom({
+	key: "VideoBtn",
+	default: false,
+});
