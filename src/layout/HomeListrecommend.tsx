14b085262f632cf26fcb2079814c0e25467d117c
jungpa <operland55@naver.com>
2022-11-16 19:29:22 +0900

    ✨ Sign Up Screen and style Component

---
+++ src/layout/HomeListrecommend.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { makeImagePath } from "../utils";
+import { Data } from "../Components/HomeListCards";
+
+function HomeListRecommend({ data, isLoading }: Data) {
+	return (
+		<div className="recommend-container">
+			<Link to={`ContentsMovie/${data?.results[0].id}`}>
+				<div className="recommend-FirstCard">
+					<img
+						src={makeImagePath(`/${data?.results[0].backdrop_path}`)}
+						alt=""
+					/>
+					<span>{data?.results[0].title}</span>
+				</div>
+			</Link>
+			<ul className="recommend-lists">
+				{isLoading ? (
+					<></>
+				) : (
+					data?.results.slice(1, 7).map((item: any, key: any) => (
+						<Link to={`ContentsMovie/${item.id}`} key={key}>
+							<li className="recommend-cards">
+								<img src={makeImagePath(`./${item.backdrop_path}`)}></img>
+								<div className="list-recommend-info">
+									<span>{item.title}</span>
+								</div>
+							</li>
+						</Link>
+					))
+				)}
+			</ul>
+		</div>
+	);
+}
+
+export default HomeListRecommend;
