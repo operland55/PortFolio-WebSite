b6cde308165a9e3419d52af075cdd240b5811a4f
jungpa <operland55@naver.com>
2022-11-23 07:49:15 +0900

    ✨ SignUp Implement Component

--- src/layout/SignUp.tsx
+++ src/layout/SignUp.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Components/Header";
 import Footer from "./Footer";
 import {
@@ -11,13 +11,54 @@ import {
 import { SiNaver } from "react-icons/si";
 import { RiKakaoTalkFill } from "react-icons/ri";
 import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { JoinFunAtom } from "../atom";
+
+interface IForm {
+	Id: string;
+	Password: string;
+	CheckPassword: string;
+}
 
 function SignUp() {
+	const [Info, setInfo] = useRecoilState(JoinFunAtom);
+	const [IdValue, setIdValue] = useState("");
+	const {
+		register,
+		handleSubmit,
+		setValue,
+		formState: { errors },
+	} = useForm<IForm>();
 	const navigate = useNavigate();
 
 	const backBtn = () => {
 		navigate(-1);
 	};
+
+	const onValid = (data: IForm) => {
+		for (const item of Info) {
+			if (item.Id === data.Id) {
+				return alert("이미있는아뒤에요");
+			}
+		}
+		setInfo((oldInfo) => {
+			return [{ Id: data.Id, Password: data.Password }, ...oldInfo];
+		});
+		alert(`회원가입을 축하해요 ${data.Id} 님`);
+		navigate("/login");
+	};
+
+	const checkBtn = () => {
+		for (const item of Info) {
+			if (IdValue.length < 5) {
+				alert("5 characters or more");
+			} else if (item.Id === IdValue) {
+				alert("it already exists");
+			}
+		}
+	};
+
 	return (
 		<>
 			<div className="sm-hidden">
@@ -39,11 +80,26 @@ function SignUp() {
 									<h1>Join now and enjoy</h1>
 								</div>
 
-								<form>
+								<form onSubmit={handleSubmit(onValid)}>
 									<div className="sign-input">
 										<div className="sign-input-id">
-											<input type="text" placeholder="Id" />
-											<button className="input-double">Overlap check</button>
+											<input
+												type="text"
+												placeholder="Id"
+												{...register("Id", {
+													required: true,
+													minLength: {
+														value: 5,
+														message: "5 characters or more",
+													},
+												})}
+												onChange={(e) => {
+													setIdValue((cur) => (cur = e.target.value));
+												}}
+											/>
+											<span className="check" onClick={checkBtn}>
+												Overlap check
+											</span>
 										</div>
 										<p>
 											Please sign up with at least 5 characters and no more than
@@ -54,14 +110,22 @@ function SignUp() {
 									<div className="sign-input">
 										<div className="sign-input-password">
 											<input
-												type="text"
-												placeholder="Please sign up within 8 characters or less"
+												{...register("Password", {
+													required: true,
+													minLength: 8,
+												})}
+												type="Password"
+												placeholder="Password"
 											/>
 										</div>
 
 										<div className="sign-input-password">
 											<input
-												type="text"
+												{...register("CheckPassword", {
+													required: true,
+													minLength: 8,
+												})}
+												type="password"
 												placeholder="Please enter your password again"
 											/>
 										</div>
