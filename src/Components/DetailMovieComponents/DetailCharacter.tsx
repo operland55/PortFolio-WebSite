0a16ac0cc6b21b6590a34b1ae5d388cddfba9784
jungpa <operland55@naver.com>
2022-11-23 16:24:23 +0900

    ✅ Typescript add

--- src/Components/DetailMovieComponents/DetailCharacter.tsx
+++ src/Components/DetailMovieComponents/DetailCharacter.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
-import { getCastMovie } from "../../api";
+import { getCastMovie, ICredits } from "../../api";
 import { makeImagePath } from "../../utils";
 
 function DetailCharacter() {
 	let { DetailId } = useParams();
-	const { data: dataCast, isLoading: CastLoading } = useQuery(
+	const { data: dataCast, isLoading: CastLoading } = useQuery<ICredits>(
 		"CastData",
 		() => {
 			return getCastMovie(DetailId);
