37020afcaa7d858184cc3c658c14e0e6a115f01d
jungpa <operland55@naver.com>
2022-12-15 16:07:38 +0900

    ✨ Make an advertisement screen  Component

--- src/layout/AdvertisementScreen.tsx
+++ src/layout/AdvertisementScreen.tsx
@@ -14,62 +14,79 @@ function AdvertisementScreen() {
 			<div className="container">
 				<div className="row">
 					<div className="col-sm-4">
-						<header className="Advertisement-header sm-only">
-							<div
-								className="header-img"
-								onClick={() => {
-									navigate("/");
-								}}
-							></div>
-
-							<p>Advertisement/Inquiry</p>
-
-							<div className="header-icon">
-								<BsSearch
+						<div className="Advertisement-container">
+							<header className="Advertisement-header sm-only">
+								<div
+									className="header-img"
 									onClick={() => {
-										navigate("/search");
+										navigate("/");
 									}}
-								/>
-								<BiMenu />
-							</div>
-						</header>
-
-						<main className="Advertisement-main">
-							<form action="summit" className="form-wrapper">
-								<div className="form-group">
-									<input type="text" placeholder="Name" />
-								</div>
-								<div className="form-group">
-									<input type="text" placeholder="PhoneNumber enter" />
-								</div>
-								<div className="form-group">
-									<input type="text" placeholder="Email enter" />
+								>
+									<img src={Logo} alt="logo" />
 								</div>
 
-								<div className="form-group">
-									<div>
-										<div>
+								<p>Advertisement/Inquiry</p>
+
+								<div className="header-icon">
+									<BsSearch
+										onClick={() => {
+											navigate("/search");
+										}}
+									/>
+									<BiMenu />
+								</div>
+							</header>
+
+							<main className="Advertisement-main">
+								{" "}
+								<p className="title">Advertisement/Inquiry</p>
+								<h1>Advertisement</h1>
+								<form action="summit" className="form-wrapper">
+									<div className="form-group">
+										<input type="text" placeholder="Name" />
+									</div>
+									<div className="form-group">
+										<input type="text" placeholder="PhoneNumber enter" />
+									</div>
+									<div className="form-group">
+										<input type="text" placeholder="Email enter" />
+									</div>
+
+									<div className="form-label">
+										<div className="form-checkbox">
 											<label htmlFor="">
-												<input type="radio" value="광고" />
+												<input type="checkbox" value="광고" />
 												<span>광고</span>
 											</label>
 										</div>
-										<div>
-											<label htmlFor="">
-												<input type="radio" value="제휴" />
+										<div className="form-checkbox">
+											<label htmlFor="name">
+												<input type="checkbox" value="제휴" />
 												<span>제휴</span>
 											</label>
 										</div>
 									</div>
-								</div>
-								<div className="form-group">
-									<input type="text" placeholder="Enter inquiry title" />
-								</div>
-								<div className="form-group">
-									<input type="text" placeholder="Enter inquiry details" />
-								</div>
-							</form>
-						</main>
+
+									<div className="form-group">
+										<input type="text" placeholder="Enter inquiry title" />
+									</div>
+									<div className="form-group">
+										<textarea placeholder="Enter inquiry details" rows={8} />
+									</div>
+
+									<div className="form-agree">
+										<input type="checkbox" value="동의" />
+										<span>
+											Information collection and consent for business inquiries
+										</span>
+									</div>
+
+									<div className="form-button">
+										<button>Send</button>
+									</div>
+								</form>
+							</main>
+						</div>
 					</div>
 				</div>
 			</div>
